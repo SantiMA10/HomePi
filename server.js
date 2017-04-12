65658fc1031bd5f158b4db6171fcffdfe48d48dd
Santiago M.A <santiagomartinagra@gmail.com>
2017-04-12 19:48:32 +0200

    feat(server): firebase
    now the app connects with firebase and writes

--- server.js
+++ server.js
@@ -2,5 +2,16 @@
  * Created by Santiago M.A. on 12/04/2017.
  */
 
-console.log("hello");
+const firebase = require("firebase-admin");
+const config= require(__dirname + "/config/config.json");
+
+const serviceAccount = require(__dirname+config.firebase.configuration_file);
+
+firebase.initializeApp({
+	credential: firebase.credential.cert(serviceAccount),
+	databaseURL: config.firebase.url
+});
+
+var ref = firebase.database().ref("/");
+ref.update({"hello" : "world"});
 
